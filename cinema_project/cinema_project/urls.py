b4249428d6575bc4574968dc8a36bd5e41d84f71
anhelinabobrynok <181272255+anhelinabobrynok@users.noreply.github.com>
2025-12-17 10:56:10 +0200

    Delete cinema_project directory

--- cinema_project/cinema_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('cinema_app.urls')),
-    path('', include('cinema_frontend.urls')),
-]
